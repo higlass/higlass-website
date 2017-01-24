d175bd287fc486ea109e1b41203505f63dbda0b4
Fritz Lekschas <service@lekschas.de>
2017-01-23 19:09:41 -0500

    Enable JSON config dropping for HiGlass

---
+++ src/assets/scripts/utils/drop-json.js
@@ -0,0 +1,33 @@
+import $ from 'domtastic';
+
+import hasParent from './has-parent';
+
+export default function dropJson(el, dropCallback) {
+  const $el = $(el);
+
+  document.addEventListener('dragenter', (event) => {
+    if (hasParent(event.target, el)) {
+      $el.addClass('is-dragging-over');
+    }
+  });
+
+  document.addEventListener('dragover', (event) => {
+    event.preventDefault();
+  });
+
+
+  document.addEventListener('dragleave', () => {
+    $el.removeClass('is-dragging-over');
+    console.log('left');
+  });
+
+  document.addEventListener('drop', (event) => {
+    event.preventDefault();
+
+    if (hasParent(event.target, el)) {
+      dropCallback(event);
+    }
+
+    $el.removeClass('is-dragging-over');
+  }, false);
+}
