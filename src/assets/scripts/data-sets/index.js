2b98e437b5c5fbb2bce9329d118d6b6b01df7c3c
Fritz Lekschas <service@lekschas.de>
2017-01-31 21:53:46 -0500

    Load data sets from external API

---
+++ src/assets/scripts/data-sets/index.js
@@ -0,0 +1,32 @@
+import { json } from 'd3';  // eslint-disable-line
+
+const url = 'http://higlass.io/api/v1/tilesets/';
+
+function render(dataSets) {
+  let html = '';
+
+  dataSets.filter(dataSet => !dataSet.private).forEach((dataSet) => {
+    html += `
+      <li class="flex-c">
+        <!-- <figure class="datasets-figure"></figure> -->
+        <div class="flex-i-g-1">
+          <h3 class="smaller">${dataSet.name}</h3>
+          ${dataSet.description ? `<p class="smaller">${dataSet.description}</p>` : ''}
+          <h4>Properties</h4>
+          <ul class="flex-c badges no-list-style properties">
+            ${dataSet.filetype ? `<li class="smaller">File type: ${dataSet.filetype}</li>` : ''}
+            ${dataSet.datatype ? `<li class="smaller">Data type: ${dataSet.datatype}</li>` : ''}
+            ${dataSet.coordSystem ? `<li class="smaller">Coordinate system: ${dataSet.coordSystem}</li>` : ''}
+          </ul>
+        </div>
+      </li>`;
+  });
+
+  document.querySelector('#data-set-list').innerHTML = html;
+}
+
+json(url, (err, dataSets) => {
+  if (err) throw err;
+
+  render(dataSets.results);
+});
