d669b648d7ff72274efa3e608cceb340496cfae2
Fritz Lekschas <service@lekschas.de>
2018-02-14 20:20:17 -0500

    Update remaining third party libs and update to latest version of rollup

--- src/assets/scripts/base/index.js
+++ src/assets/scripts/base/index.js
@@ -1,21 +1,20 @@
 import $ from 'domtastic';
 import MenuToggler from './menu-toggler';
 
-export function disableDarkTheme() {
-  if (window.localStorage.getItem('higlass.disableDarkTheme')) {
-    window.localStorage.removeItem('higlass.disableDarkTheme');
-    $(document.body).addClass('dark-theme');
-    $(document.body).removeClass('dark-theme-disabled');
-  } else {
-    window.localStorage.setItem('higlass.disableDarkTheme', 1);
-    $(document.body).removeClass('dark-theme');
-    $(document.body).addClass('dark-theme-disabled');
-  }
+export function lightsOff() {
+  window.localStorage.setItem('hgw.darkTheme', 1);
+  $(document.body).addClass('dark-theme');
+}
+
+export function lightsOn() {
+  window.localStorage.removeItem('hgw.darkTheme');
+  $(document.body).removeClass('dark-theme');
 }
 
 /* eslint-disable no-unused-vars */
 const toggler = new MenuToggler(document.querySelector('#topbar .menu-toggler'));
 
 export default {
-  disableDarkTheme
+  lightsOff,
+  lightsOn
 };
