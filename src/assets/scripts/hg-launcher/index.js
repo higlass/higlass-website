4b077eb966eca29850b978e971de9fc665d8f912
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-04-11 12:03:57 -0400

    Take out protocol from viewconf requests

--- src/assets/scripts/hg-launcher/index.js
+++ src/assets/scripts/hg-launcher/index.js
@@ -45,13 +45,13 @@ const viewconfId = query.config ? query.config : 'default';
 
 launchHg('#higlass', `/api/v1/viewconfs/?d=${viewconfId}`, true); // TODO: Graceful fallback if no viewconf with this ID?
 
-launchHg('#higlass1', 'http://higlass.io/api/v1/viewconfs/?d=default', true); // TODO: Graceful fallback if no viewconf with this ID?
-launchHg('#higlass2', 'http://higlass.io/api/v1/viewconfs/?d=twoviews', true); // TODO: Graceful fallback if no viewconf with this ID?
-launchHg('#higlass3', 'http://higlass.io/api/v1/viewconfs/?d=browserlike', false); // TODO: Graceful fallback if no viewconf with this ID?
-launchHg('#higlass4', 'http://higlass.io/api/v1/viewconfs/?d=browserwithdetails', false); // TODO: Graceful fallback if no viewconf with this ID?
+launchHg('#higlass1', '//higlass.io/api/v1/viewconfs/?d=default', true); // TODO: Graceful fallback if no viewconf with this ID?
+launchHg('#higlass2', '//higlass.io/api/v1/viewconfs/?d=twoviews', true); // TODO: Graceful fallback if no viewconf with this ID?
+launchHg('#higlass3', '//higlass.io/api/v1/viewconfs/?d=browserlike', false); // TODO: Graceful fallback if no viewconf with this ID?
+launchHg('#higlass4', '//higlass.io/api/v1/viewconfs/?d=browserwithdetails', false); // TODO: Graceful fallback if no viewconf with this ID?
 
 
-launchHg('#higlass_test1', 'http://test.higlass.io/api/v1/viewconfs/?d=test_default', true); // TODO: Graceful fallback if no viewconf with this ID?
-launchHg('#higlass_test2', 'http://test.higlass.io/api/v1/viewconfs/?d=test_twoviews', true); // TODO: Graceful fallback if no viewconf with this ID?
-launchHg('#higlass_test3', 'http://test.higlass.io/api/v1/viewconfs/?d=test_browserlike', false); // TODO: Graceful fallback if no viewconf with this ID?
-launchHg('#higlass_test4', 'http://test.higlass.io/api/v1/viewconfs/?d=test_browserwithdetails', false); // TODO: Graceful fallback if no viewconf with this ID?
+launchHg('#higlass_test1', '//test.higlass.io/api/v1/viewconfs/?d=test_default', true); // TODO: Graceful fallback if no viewconf with this ID?
+launchHg('#higlass_test2', '//test.higlass.io/api/v1/viewconfs/?d=test_twoviews', true); // TODO: Graceful fallback if no viewconf with this ID?
+launchHg('#higlass_test3', '//test.higlass.io/api/v1/viewconfs/?d=test_browserlike', false); // TODO: Graceful fallback if no viewconf with this ID?
+launchHg('#higlass_test4', '//test.higlass.io/api/v1/viewconfs/?d=test_browserwithdetails', false); // TODO: Graceful fallback if no viewconf with this ID?
